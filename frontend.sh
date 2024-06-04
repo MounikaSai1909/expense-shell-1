6a3420c85b7a8bde080ae89b5a61792d6e3b4871
MounikaSai1909 <mounikasaiaarepu09@gmail.com>
2024-06-04 13:44:26 +0530

    some-message

--- frontend.sh
+++ frontend.sh
@@ -3,26 +3,26 @@ source ./common.sh
 
 check_root
 
-dnf install nginx -y &>> $LOGFILE
+dnf install nginx -y &>> $LOG_FILE
 VALIDATE $? "Installing nginx"
 
-systemctl enable nginx &>> $LOGFILE
+systemctl enable nginx &>> $LOG_FILE
 VALIDATE $? "Enabling nginx"
 
-systemctl start nginx &>> $LOGFILE
+systemctl start nginx &>> $LOG_FILE
 VALIDATE $? "Starting nginx"
 
-rm -rf /usr/share/nginx/html/* &>> $LOGFILE
+rm -rf /usr/share/nginx/html/* &>> $LOG_FILE
 VALIDATE $? "Removing Existing Content"
 
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>> $LOGFILE
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>> $LOG_FILE
 VALIDATE $? "Downloading Frontend Code"
 
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>> $LOGFILE
+unzip /tmp/frontend.zip &>> $LOG_FILE
 VALIDATE $? "unzipping frontend code"
 
-systemctl restart nginx &>> $LOGFILE
+systemctl restart nginx &>> $LOG_FILE
 VALIDATE $? "Restarting nginx"
 
 
